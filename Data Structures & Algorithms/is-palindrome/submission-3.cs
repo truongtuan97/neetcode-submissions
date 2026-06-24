cc1571192ad99b07bdcd966f15ef2923d44a254d
truongtuan97 <truongtuan97@yahoo.com>
2026-06-24 18:59:31 +0700

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public bool IsPalindrome(string s) {
+        string cleanString = Regex.Replace(s, @"[^a-zA-Z0-9]", "").ToLower();
+        int left = 0;
+        int right = cleanString.Length - 1;
+
+        while (left < right) {
+            if (cleanString[left] != cleanString[right]) {                
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
